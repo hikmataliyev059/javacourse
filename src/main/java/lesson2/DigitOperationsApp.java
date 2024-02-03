ab3238f792932016a99931968f5e4db4e6b72c7c
hikmataliyev059 <155654977+hikmataliyev059@users.noreply.github.com>
2024-02-04 02:28:58 +0400

    Update DigitOperationsApp.java

--- src/main/java/lesson2/DigitOperationsApp.java
+++ src/main/java/lesson2/DigitOperationsApp.java
@@ -2,8 +2,11 @@ package lesson2;
  import java.util.Scanner;
   public class DigitOperationsApp {
     public static void main(String[] args) {
+        // 1.input
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
+
+        // 2.Process
         int count = 0;
         int sum = 0;
         int prod = 1;
@@ -14,6 +17,7 @@ package lesson2;
             sum += rem;
             prod *= rem;
         }
+        // 3.Output
         System.out.printf("Count of digits is:%d\n", count);
         System.out.printf("Sum of digits is:%d\n", sum);
         System.out.printf("Prod of digits is:%d\n", prod);
