ce666b2baba7dbd22f5dca379ae16d132b92dd24
hikmet059 <javahikmet5909@gmail.com>
2024-02-03 23:45:12 +0400

    numberSumApp

---
+++ src/main/java/lesson2/numberSumApp.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class numberSumApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 1;
+        for (int i = 0; i < 3; i++) {
+            int a = num % 10;
+            num = num / 10;
+            sum = sum * a;
+        }
+        System.out.println(sum);
+    }
+}
