8e413756aefa66dd279cd3bd8f75f8b50a9859f9
hikmet059 <javahikmet5909@gmail.com>
2024-02-04 22:03:24 +0400

    EvenorOddApp

---
+++ src/main/java/lesson2/EvenorOddApp.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class EvenorOddApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
