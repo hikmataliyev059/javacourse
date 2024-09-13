ce0eb4db764b150cdb2c602682dd636957226653
Hikmat Aliyev <155654977+hikmataliyev059@users.noreply.github.com>
2024-09-13 19:14:09 +0400

    Update Main.java

--- src/main/java/az/classes/lesson02/Main.java
+++ src/main/java/az/classes/lesson02/Main.java
@@ -1,14 +1,13 @@
 package az.classes.lesson02;
 
 public class Main {
+ 
  public  static  void  main(String[] args){
      System.out.print("Hello, World!");
      System.out.println("Hello, World!");
      System.out.printf("Hello, %s", "World!");
      System.out.printf("Hello, %s\n", "World!");
      System.out.printf("Hello, World: %.5f", 67800.056742);
-
-
-
- }
+   }
+ 
 }
